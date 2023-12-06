d6135f551d1c8fc823c4e9461441ebeeaa5d0b21
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2023-12-06 20:36:26 -0300

    Add files via upload

--- src/server.jsx
+++ src/server.jsx
@@ -6,9 +6,19 @@ const Mercado_Pago = require('./router/Mercado_Pago_Router.jsx');
 
 const server = express();
 
-//*- proxy - middleware :
+// Configuração do CORS
+const corsOptions = {
+  origin: 'http://localhost:5173', // Substitua pelo seu domínio de frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+// Aplicação do middleware do cors
+server.use(cors(corsOptions));
+
+// Configuração das rotas
 server.use(express.json());
-server.use(cors());
 server.use('/Mercado_Pago', Mercado_Pago);
 
 module.exports = server;
